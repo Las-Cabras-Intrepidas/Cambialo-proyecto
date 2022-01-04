108d4788c89b8afdc0163a7444b6941729113786
Soleny Yumare <solenyyumare@gmail.com>
2022-01-04 20:55:45 +0000

    Añadido archivo json, el proceso de creación de producto y la subida funciona y se adjunta al html de product

---
+++ src/js/form-upload-product.js
@@ -0,0 +1,58 @@
+let listItemsUploaded = [];
+const termsConditions = document.querySelector("#terms");
+//revisar este prevent default
+termsConditions.addEventListener("change", (e) => e.preventDefault());
+
+const btnSubmit = document.querySelector(".btn");
+btnSubmit.addEventListener("click", sendForm);
+
+// upload de los productos
+function show() {
+  const archivo = document.getElementById("file").files[0];
+  const reader = new FileReader();
+  // eslint-disable-next-line no-undef
+  if (file) {
+    reader.readAsDataURL(archivo);
+    reader.onloadend = function () {
+      //console.log(reader.result);
+      document.getElementById("img").src = reader.result;
+    };
+  }
+}
+
+class Product {
+  constructor(title, description, picture, availability = true) {
+    this.title = title;
+    this.description = description;
+    this.picture = picture;
+
+    this.availability = availability ? "Disponible" : "No Disponible";
+  }
+}
+const saveLocalStorage = (title, product) =>
+  localStorage.setItem(title, JSON.stringify(product));
+// Item = array de los nombres de los productos que se suben
+// Para despues poder pedir el resto de items
+function sendForm() {
+  const img = document.getElementById("img").src;
+  const megaTitle = document.getElementById("product-title").value;
+  const megaDescription = document.getElementById("text-upload").value;
+
+  const product = new Product(megaTitle, megaDescription, img);
+  listItemsUploaded.push(megaTitle);
+  saveLocalStorage("Lista de productos", listItemsUploaded);
+  // product.picture = img;
+  // product.title = megaTitle;
+  // product.description = megaDescription;
+  saveLocalStorage(megaTitle, product);
+  // addProductToCatalog(product);
+  console.log(product);
+}
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("Lista de Productos") != null) {
+    let listUploadProduct = JSON.parse(
+      localStorage.getItem("Lista de Productos")
+    );
+    listItemsUploaded = listUploadProduct;
+  }
+});
--- src/js/upload-product.js
+++ src/js/upload-product.js
@@ -1,30 +1,22 @@
 /* eslint-disable quote-props */
 /* eslint-disable quotes */
-function show() {
-  const archivo = document.getElementById("file").files[0];
-  const reader = new FileReader();
-  // eslint-disable-next-line no-undef
-  if (file) {
-    reader.readAsDataURL(archivo);
-    reader.onloadend = function () {
-      console.log(reader.result);
-      document.getElementById("img").src = reader.result;
-    };
-  }
-}
+//  Json de los productos
+let listProduct = [];
 
-const termsConditions = document.querySelector("#terms");
-termsConditions.addEventListener("change", (e) => e.preventDefault());
+// ! const termsConditions = document.querySelector("#terms");
+//revisar este prevent default
+// ! termsConditions.addEventListener("change", (e) => e.preventDefault());
 
-const btnSubmit = document.querySelector(".btn");
-btnSubmit.addEventListener("click", sendForm);
+/* const btnSubmit = document.querySelector(".btn");
+btnSubmit.addEventListener("click", sendForm);*/
 
 class Product {
-  constructor(title, description, picture) {
-    (this.title = title),
-      (this.description = description),
-      (this.picture = picture),
-      (this.availability = "Disponible");
+  constructor(title, description, picture, availability = true) {
+    this.title = title;
+    this.description = description;
+    this.picture = picture;
+
+    this.availability = availability ? "Disponible" : "No Disponible";
   }
 }
 
@@ -33,27 +25,28 @@ function sendForm() {
   const megaTitle = document.getElementById("product-title").value;
   const megaDescription = document.getElementById("text-upload").value;
 
-  const product = new Product();
+  const product = new Product(megaTitle, megaDescription, img);
 
-  product.picture = img;
-  product.title = megaTitle;
-  product.description = megaDescription;
+  // product.picture = img;
+  // product.title = megaTitle;
+  // product.description = megaDescription;
+  listProduct.push(product);
+  // addProductToCatalog(product);
 
+  console.log(product);
+}
+
+function addProductToCatalog(product, container) {
   const classes = {
+    divCard: "column is-half-touch is-one-quarter-desktop",
     card: "product-card",
     paragraph: "has-text-weight-bold",
     description: "has-text-left",
     availability: "green",
     text: "text-product",
   };
-
-  addProductToCatalog(product, classes);
-
-  console.log(product);
-}
-
-function addProductToCatalog(product, classes) {
   const template = `
+  <div class="${classes.divCard}">
     <div class="${classes.card}">
       <img src="${product.picture}" alt="${product.title}">
       <div class="${classes.text}">
@@ -62,10 +55,50 @@ function addProductToCatalog(product, classes) {
         <p class="${classes.description}">${product.description}</p>
       </div>
     </div>
+  </div>
   `;
-
-  const catalog = document.querySelector("#producto");
-  catalog.insertAdjacentHTML("beforeEnd", template);
+  // const catalog = document.querySelector("#producto");
+  container.insertAdjacentHTML("beforeEnd", template);
 }
+function createProduct() {
+  fetch("js/producto.json")
+    .then((response) => response.json())
+    .catch((error) => console.error("Esto no funciona", error))
+    .then((data) => {
+      // Array de los productos del JSON
 
-// como guardar datos recogidos del form y hacer un json con ellos
+      for (let index in data) {
+        listProduct.push(data[index]);
+      }
+      console.log(listProduct);
+      createAllProduct(listProduct);
+    });
+}
+function createAllProduct(ProductList) {
+  const containerProduct = document.querySelector(".product-container");
+  for (let index in ProductList) {
+    const ItemJSON = ProductList[index];
+    const Product1 = new Product(
+      ItemJSON.title,
+      ItemJSON.description,
+      ItemJSON.picture,
+      ItemJSON.available
+    );
+    addProductToCatalog(Product1, containerProduct);
+  }
+}
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("Lista de productos") != null) {
+    let listUploadProduct = JSON.parse(
+      localStorage.getItem("Lista de productos")
+    );
+    for (let index in listUploadProduct) {
+      const UploadProduct = JSON.parse(
+        localStorage.getItem(listUploadProduct[index])
+      );
+      console.log(UploadProduct);
+      listProduct.push(UploadProduct);
+    }
+  }
+  createProduct();
+});
